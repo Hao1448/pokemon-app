9918bd1aa5b0efb8b327db8d51c4a3429922ca89
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-05-20 18:21:47 +0300

    Add fonts

---
+++ base/fonts.js
@@ -0,0 +1,13 @@
+export const GlobalFonts = `
+    @font-face {
+        font-family: 'Cormorant';
+        src: url('/static/fonts/Cormorant/Cormorant-Regular.eot');
+        src: url('/static/fonts/Cormorant/Cormorant-Regular.eot?#iefix') format('embedded-opentype'),
+            url('/static/fonts/Cormorant/Cormorant-Regular.woff2') format('woff2'),
+            url('/static/fonts/Cormorant/Cormorant-Regular.woff') format('woff'),
+            url('/static/fonts/Cormorant/Cormorant-Regular.ttf') format('truetype'),
+            url('/static/fonts/Cormorant/Cormorant-Regular.svg#Cormorant-Regular') format('svg');
+        font-weight: normal;
+        font-style: normal;
+    }
+`
