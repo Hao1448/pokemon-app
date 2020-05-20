a8121fc2012f514b1a4f365e9e5015180e3e4ba0
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-05-20 20:20:27 +0300

    Mod index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
-import { CardPokemon } from '../components'
+import styled, { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../base/styles'
+import { GlobalTheme } from '../base/theme'
+import { Header } from '../components'
 
 
 export default function Home() {
   return (
-    <CardPokemon/>
+    <AppWrapper>
+      <ThemeProvider theme={GlobalTheme}>
+        <Wrapper>
+          <GlobalStyle/>
+          <Header/>
+        </Wrapper>
+      </ThemeProvider>
+    </AppWrapper>
   )
 }
+
+const AppWrapper = styled.div``
+
+const Wrapper = styled.div``
