cce268176a82d629de27ac4d434c72f5ba9b09a8
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-05-21 17:44:29 +0300

    Update CardPokemon

--- components/CardPokemon/index.js
+++ components/CardPokemon/index.js
@@ -1,24 +1,49 @@
 import styled from 'styled-components'
+import { h2 } from '../../base/mixins/text'
+import Link from 'next/link';
 
-const CardPokemon = () => {
+const CardPokemon = ({name, id, img}) => {
     return (
         <Wrapper>
-            <Image/>
-            <Title>Pickachu</Title>
+         <Link href={`/pokemon/${id}`}>
+         <LinkWrapper>
+            <Image src={img}/>
+            <Title>
+                <Text>
+                    <a>{name}</a>
+                </Text>
+            </Title>
+            </LinkWrapper>
+            </Link>
         </Wrapper>
     )
+
 }
 
 const Wrapper = styled.div`
-    height: 200px;
-    width: 200px;
-    background-color: red;
+    cursor: pointer;
+    border-radius: 20px;
+    padding: 10px;
+    transition: box-shadow 0.2s;
+    &:hover {
+        box-shadow: 0 0 10px 5px  ${p => p.theme.color.secondary};
+    }
 `
 
 const Title = styled.div`
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
-const Image = styled.img``
+const Text = styled.div`
+    ${h2};
+    color: ${p => p.theme.color.primary};
+    margin: 0;
+`
 
+const Image = styled.img`
+    max-width: 100%;
+`
+const LinkWrapper = styled.div``
 export default CardPokemon
